f73b21007ba1677f30d1adcbc2d9a49df15a814c
bcochran <b.cochrane@imperial.ac.uk>
2017-03-13 14:35:56 +0000

    metricbeat

---
+++ mbeat.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# removed as filebeat installs repository
+#cat <<EOT >> /etc/yum.repos.d/elasticsearch.repo
+#[elastic-5.x]
+#name=Elastic repository for 5.x packages
+#baseurl=https://artifacts.elastic.co/packages/5.x/yum
+#gpgcheck=1
+#gpgkey=https://artifacts.elastic.co/GPG-KEY-elasticsearch
+#enabled=1
+#autorefresh=1
+#type=rpm-md
+#EOT
+#yum upgrade -y
+yum install metricbeat -y
+sed -i 's/hosts\: \[\"localhost\:9200\"\]/hosts\: \[\"ee-elk.ee.ic.ac.uk\:9200\"\]/g' /etc/metricbeat/metricbeat.yml
+#systemctl start filebeat
+chkconfig metricbeat on
+service metricbeat start
+
