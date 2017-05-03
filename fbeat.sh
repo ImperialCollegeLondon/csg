34b20e90d8f61c8b2fb93d3e1a5721a7d5d461b6
bcochran <b.cochrane@imperial.ac.uk>
2017-05-03 18:51:10 +0100

    ipv6

--- fbeat.sh
+++ fbeat.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+# Disable IPV6 due to DNS problems
+sysctl -w net.ipv6.conf.all.disable_ipv6=1
+sysctl -w net.ipv6.conf.default.disable_ipv6=1
+# Create Repo
 cat <<EOT >> /etc/yum.repos.d/elasticsearch.repo
 [elastic-5.x]
 name=Elastic repository for 5.x packages
@@ -10,9 +14,13 @@ autorefresh=1
 type=rpm-md
 EOT
 yum upgrade -y
+# Install
 yum install filebeat -y
+# Configure
 sed -i 's/hosts\: \[\"localhost\:9200\"\]/hosts\: \[\"ee-elk.ee.ic.ac.uk\:9200\"\]/g' /etc/filebeat/filebeat.yml
 #systemctl start filebeat
+# Enable Service
 chkconfig filebeat on
+# Start Service
 service filebeat start
 
