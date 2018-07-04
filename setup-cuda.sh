fbb5f0c679f2574ece0ef280dd27ca5294ff9b1a
bcochran <b.cochrane@imperial.ac.uk>
2018-07-04 17:59:46 +0100

    cuda

--- setup-cuda.sh
+++ setup-cuda.sh
@@ -14,12 +14,19 @@ if [ ! -f /etc/redhat-release ]; then
         exit
 fi
 
+# Disable Opensource Drivers
 rpm -e xorg-x11-drivers xorg-x11-drv-nouveau
 echo -e "blacklist nouveau\nblacklist lbm-nouveau\noptions nouveau modeset=0\nalias nouveau off\nalias lbm-nouveau off" | tee /etc/modprobe.d/blacklist-nouveau.conf > /dev/null
 echo options nouveau modeset=0 | sudo tee /etc/modprobe.d/nouveau-kms.conf > /dev/null
 update-initramfs -u
+
+# Install Requirements
 yum -y groupinstall "Development Tools"
 yum -y install kernel-devel
+
+# Add Nvidia Repo
 rpm -Uvh https://developer.download.nvidia.com/compute/cuda/repos/rhel7/x86_64/cuda-repo-rhel7-9.2.88-1.x86_64.rpm
 yum clean all
+
+# Install CUDA
 yun install cuda -y
