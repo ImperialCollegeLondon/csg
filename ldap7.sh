02a0ab0d50e29934e862ba23b93d39aaa56720c6
bcochran <b.cochrane@imperial.ac.uk>
2018-08-10 13:25:45 +0100

    change error handling

--- ldap7.sh
+++ ldap7.sh
@@ -1,6 +1,8 @@
-#!/bin/bash
-set -x
-set -e
+#!/bin/env bash
+#set -x
+#set -e
+# Import Bash OO Framework
+source "$( cd "${BASH_SOURCE[0]%/*}" && pwd )/lib/oo-bootstrap.sh"
 
 # Make sure only root can run our script
 if [ "$(id -u)" != "0" ]; then
