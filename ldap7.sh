f332e7a51afa009cf92c3c589f524209d7456944
Bryan Cochrane <bcochran@ness.doc.ic.ac.uk>
2018-11-29 12:01:30 +0000

    puppet

--- ldap7.sh
+++ ldap7.sh
@@ -2,30 +2,28 @@
 #set -x
 #set -e
 # Import Bash OO Framework
-source "$( cd "${BASH_SOURCE[0]%/*}" && pwd )/lib/oo-bootstrap.sh"
+#source "$( cd "${BASH_SOURCE[0]%/*}" && pwd )/lib/oo-bootstrap.sh"
 
 # Make sure only root can run our script
-if [ "$(id -u)" != "0" ]; then
-   echo "This script must be run as root" 1>&2
-   exit 1
-fi
+#if [ "$(id -u)" != "0" ]; then
+#   echo "This script must be run as root" 1>&2
+#   exit 1
+#fi
 
 # Check to make sure script is running on CentOS/Redhat
-if [ ! -f /etc/redhat-release ]; then
-        echo "This script is for CentOS"
-        exit
-fi
+#if [ ! -f /etc/redhat-release ]; then
+#        echo "This script is for CentOS"
+#        exit
+#fi
 
 echo Installing LDAP and Kerberos clients
 yum install wget vim deltarpm git epel-release -y
 yum upgrade -y
-rpm -Uvh http://li.nux.ro/download/nux/dextop/el7/x86_64/nux-dextop-release-0-5.el7.nux.noarch.rpm
+#rpm -Uvh http://li.nux.ro/download/nux/dextop/el7/x86_64/nux-dextop-release-0-5.el7.nux.noarch.rpm
 yum install man samba-client samba-common cifs-utils nfs-utils nfs-utils-lib epel-release wget ntp openldap-clients nss-pam-ldapd krb5-workstation pam_krb5 net-tools authconfig krb5-libs oddjob-mkhomedir pam_ldap sssd oddjob -y
 service nfs start
 service rpcbind start
 
-
-yum update -y
 yum install pam_mount -y
 echo "INFO: setup-authentication.sh: Switch over to LDAP and Kerberos 5"
 ADSERVERS=icads34.ic.ac.uk:88,icads12.ic.ac.uk:88,icads13.ic.ac.uk:88,icads14.ic.ac.uk:88,icads36.ic.ac.uk:88,icads35.ic.ac.uk:88,icads15.ic.ac.uk:88
