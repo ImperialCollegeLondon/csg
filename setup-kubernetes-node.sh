af769f7a4d1dc121208d0ad980fd007d573e4123
Bryan Cochrane <bcochran@ness.doc.ic.ac.uk>
2019-01-11 17:58:24 +0000

    kubernetes node install

--- setup-kubernetes-node.sh
+++ setup-kubernetes-node.sh
@@ -19,11 +19,11 @@ swapoff -a
 sed -i.bak '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab
 
 # Configure FirewallD
-wget -P /etc/firewalld/services https://raw.githubusercontent.com/wrightrocket/k8s-firewalld/master/k8s-master.xml
+wget -P /etc/firewalld/services https://raw.githubusercontent.com/wrightrocket/k8s-firewalld/master/k8s-worker.xml
 systemctl enable firewalld
 systemctl start firewalld
 firewall-cmd --reload
-firewall-cmd --add-service k8s-master --permanent
+firewall-cmd --add-service k8s-worker --permanent
 firewall-cmd --reload
 
 # Kubernetes Repo
@@ -54,24 +54,27 @@ EOF
 sysctl --system
 
 # Run on master node only!
-kubeadm init --pod-network-cidr=10.244.0.0/16
-mkdir -p $HOME/.kube
-cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
-chown $(id -u):$(id -g) $HOME/.kube/config
+#kubeadm init --pod-network-cidr=10.244.0.0/16
+#mkdir -p $HOME/.kube
+#cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+#chown $(id -u):$(id -g) $HOME/.kube/config
 
 # Verify node is ready
-kubectl get nodes
-read -p "Press [Enter] to confirm"
+#kubectl get nodes
+#read -p "Press [Enter] to confirm"
 
 # Verify kube-system pods are ready
-kubectl get pods --all-namespaces
-read -p "Press [Enter] to confirm"
+#kubectl get pods --all-namespaces
+#read -p "Press [Enter] to confirm"
 
 # Install Flannel networking
-kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/bc79dd1505b0c8681ece4de4c0d86c5cd2643275/Documentation/kube-flannel.yml
+#kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/bc79dd1505b0c8681ece4de4c0d86c5cd2643275/Documentation/kube-flannel.yml
 
 # Retrieve setup token
-kubeadm token list
+#kubeadm token list
 
 # Master is ready
-echo Master is installed.
+echo Join node to cluster with token.
+echo kubeadm join --token <token> vm-k8s-01.doc.ic.ac.uk6443
+echo on Kubernetes master use kubeadm token list to fetch token
+echo If there is no current token create one with kubeadm token create --ttl 3600
