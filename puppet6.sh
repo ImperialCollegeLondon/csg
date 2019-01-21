f6ad10ef20082466040eecfb83808b6920cb34ee
Bryan Cochrane <bcochran@ness.doc.ic.ac.uk>
2019-01-21 16:12:40 +0000

    Puppet install script

--- puppet6.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-rpm -Uvh https://yum.puppetlabs.com/puppetlabs-release-pc1-el-6.noarch.rpm
-yum install puppet-agent -y
-echo server=vm-puppet.doc.ic.ac.uk >> /etc/puppetlabs/puppet/puppet.conf
-/opt/puppetlabs/puppet/bin/puppet agent -t
-#systemctl enable puppet
-chkconfig puppet on
-#systemctl start puppet
-service puppet start
